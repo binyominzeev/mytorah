d757ec29db7a062a9dea452f3d78a2bc620c7272
binyominzeev <szvbinjomin@gmail.com>
2026-09-08 11:35:09 +0200

    refactor

--- upload.py
+++
@@ -1,9 +0,0 @@
-from utils.site_generator import generate_html
-from utils.ftp_upload import upload_to_ftp
-import sys
-
-if __name__ == "__main__":
-    dry_run = "--dry-run" in sys.argv[1:]
-    generate_html()
-    upload_to_ftp(dry_run=dry_run)
-
